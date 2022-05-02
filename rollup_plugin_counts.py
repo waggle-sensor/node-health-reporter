b82cd2dc6a92e65f85723817849f6118cc72f1a6
nconrad <1194246+nconrad@users.noreply.github.com>
2022-05-02 10:57:47 -0500

    add plugin count script

---
+++ rollup_plugin_counts.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+import pandas as pd
+import logging
+import sage_data_client
+import json
+
+from utils import load_node_table, parse_time, get_rollup_range, time_windows, write_results_to_influxdb
+
+
+
+def get_plugin_counts_for_window(nodes, start, end, convert_timestamps=False):
+    df = sage_data_client.query(start=start, end=end, filter={
+        "plugin": ".*"
+    })
+
+    df["timestamp"] = df["timestamp"].dt.round("1h")
+    df["total"] = 1
+
+    # ignore records with "plugin.duration" for now
+    df = df[df["name"].str.contains("plugin.duration") == False]
+
+    table = df.groupby(["meta.node", "meta.vsn", 'meta.plugin'])[["total"]].sum()
+
+    records = []
+    for node in nodes:
+        try:
+            r = table.loc[(node.id, node.vsn)]
+            plugin_totals =  r["total"]
+        except KeyError:
+            continue # ignore if id & vsn combination not found
+
+        for plugin_name, total in plugin_totals.items():
+            records.append({
+                "measurement": "total",
+                "tags": {
+                    "vsn": node.vsn,
+                    "node": node.id,
+                    "plugin": plugin_name
+                },
+                "fields": {
+                    "value": int(total), # is this necessary?
+                },
+                "timestamp": start.isoformat()+'Z' if convert_timestamps else start,
+            })
+
+    return records
+
+
+
+def main():
+    INFLUXDB_URL = "https://influxdb.sagecontinuum.org"
+    INFLUXDB_ORG = "waggle"
+    INFLUXDB_TOKEN = os.environ["INFLUXDB_TOKEN"]
+
+    now = pd.to_datetime("now", utc=True)
+    def time_arg(s):
+        return parse_time(s, now=now)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start", default="-2h", type=time_arg, help="relative start time")
+    parser.add_argument("--end", default="-1h", type=time_arg, help="relative end time")
+    parser.add_argument("--window", default="1h", type=pd.Timedelta, help="window duration to aggreagate over")
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S")
+
+    nodes = load_node_table()
+    start, end = get_rollup_range(args.start, args.end)
+    window = args.window
+
+    logging.info("current time is %s", now)
+
+    for start, end in time_windows(start, end, window):
+        logging.info("getting plugin counts for %s %s", start, end)
+        records = get_plugin_counts_for_window(nodes, start, end)
+
+        # print(json.dumps(records))
+
+        logging.info("writing %d sanity health records...", len(records))
+        write_results_to_influxdb(
+            url=INFLUXDB_URL,
+            org=INFLUXDB_ORG,
+            token=INFLUXDB_TOKEN,
+            bucket="downsampled-test",
+            records=records)
+
+    logging.info("done!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,103 @@
+
+import influxdb_client
+from influxdb_client.client.write_api import WriteOptions, WritePrecision, Point, WriteType
+import pandas as pd
+import requests
+from typing import NamedTuple
+
+
+def write_results_to_influxdb(url, token, org, bucket, records):
+    data = []
+
+    for r in records:
+        p = Point(r["measurement"])
+        for k, v in r["tags"].items():
+            p = p.tag(k, v)
+        for k, v in r["fields"].items():
+            p = p.field(k, v)
+        p = p.time(int(r["timestamp"].timestamp()), write_precision=WritePrecision.S)
+        data.append(p)
+
+    with influxdb_client.InfluxDBClient(url=url, token=token, org=org) as client, \
+         client.write_api(write_options=WriteOptions(batch_size=10000)) as write_api:
+        write_api.write(bucket=bucket, org=org, record=data, write_precision=WritePrecision.S)
+
+
+def check_publishing_frequency(df, freq, window):
+    total_samples = (df.resample(freq, on="timestamp").value.count() > 0).sum()
+    expected_samples = window / pd.Timedelta(freq)
+    return total_samples / expected_samples
+
+
+
+class Node(NamedTuple):
+    id: str
+    vsn: str
+    type: str
+    devices: set
+
+
+def load_node_table():
+    r = requests.get("https://api.sagecontinuum.org/production")
+    r.raise_for_status()
+    return [load_node_table_item(item) for item in r.json() if item["vsn"] != ""]
+
+
+def load_node_table_item(item):
+    node_type = item["node_type"].lower()
+    devices = set()
+    if node_type == "wsn":
+        devices.add("nxcore")
+        devices.add("bme280")
+    if node_type == "dell":
+        devices.add("dell")
+    if item["nx_agent"] is True:
+        devices.add("nxagent")
+    if item["shield"] is True:
+        devices.add("rpi")
+    if item["shield"] is True:
+        devices.add("raingauge")
+    if item["shield"] is True:
+        devices.add("bme680")
+    if item["shield"] is True:
+        devices.add("microphone")
+
+    # add cameras
+    for dir in ["top", "bottom", "left", "right"]:
+        if item[f"{dir}_camera"] not in [None, "", "none"]:
+            devices.add(f"{dir}_camera")
+
+    # TODO add camera stuff for upload checks
+    return Node(
+        id=item["node_id"].lower(),
+        vsn=item["vsn"].upper(),
+        type=node_type,
+        devices=devices,
+    )
+
+
+def time_windows(start, end, freq):
+    windows = pd.date_range(start, end, freq=freq)
+    return zip(windows[:-1], windows[1:])
+
+
+def parse_time(s, now=None):
+    try:
+        return pd.to_datetime(s, utc=True)
+    except ValueError:
+        pass
+    if now is None:
+        now = pd.to_datetime("now", utc=True)
+    try:
+        return now + pd.to_timedelta(s)
+    except ValueError:
+        pass
+    raise ValueError("invalid time format")
+
+
+def get_rollup_range(start, end, now=None):
+    if now is None:
+        now = pd.to_datetime("now", utc=True)
+    return start.floor("1h"), end.floor("1h")
+
+
